ed3606fdd2a1b50b23bc17e7ea606aadb8ab0c21
theara <theara729@gmail.com>
2020-09-01 22:25:38 +0700

    tested, working

--- shiftRegister4094SevenSeg.cpp
+++ shiftRegister4094SevenSeg.cpp
@@ -3,6 +3,7 @@
 #include "Arduino.h"
 #include "shiftRegister4094SevenSeg.h"
 #include "CharacterEncoding.h"
+#include <string.h>
 
 #define SEG_LETTER_INDEX 10
 #define SEG_BLANK_ARRAY ' '
@@ -10,6 +11,10 @@
 #define SEG_DASH_INDEX 37
 #define SEG_PERIOD_INDEX 38
 #define SEG_DEGREE_INDEX 39
+
+#define IDX_NUMBER 48
+#define IDX_UPPER_CASE 65
+#define IDX_LOWER_CASE 97
 
 
 const byte digitCodeMap[] = {
@@ -52,7 +57,7 @@ const byte digitCodeMap[] = {
   	0b01011011, // 90  'Z'  Same as '2'
   	0b00000000, // 32  ' '  BLANK
   	0b01000000, // 45  '-'  DASH
-  	0b10000000, // 46  '.'  PERIOD
+  	0b010000000, // 46  '.'  PERIOD
   	0b01100011, // 42  '*'  DEGREE ..
 };
 
@@ -71,137 +76,99 @@ void shiftRegister4094SevenSeg::begin(uint8_t data_pin, uint8_t clock_pin, uint8
 }
 
 void shiftRegister4094SevenSeg::print(int _val){
-	String _str = (String) _val;
-	print(_str);
+	print((String) _val);
 }
 
 void shiftRegister4094SevenSeg::print(long _val){
-	String _str = (String) _val;
-	print(_str);
+	print((String) _val);
 }
 
 void shiftRegister4094SevenSeg::print(float _val){
-	String _str = (String) _val;
-	print(_str);
+	print((String) _val);
 }
 
 void shiftRegister4094SevenSeg::print(char _ch){
-	clearBuffer();
-	setBufferArray(_digit_num-1, _ch);
+	print((String) _ch);
 }
 
 void shiftRegister4094SevenSeg::print(String _str){
-	clearBuffer();
 	char cha_buf[_str.length()];
-	_str.toCharArray(cha_buf, _str.length());
-	setBufferArray(_digit_num-_str.length(), _digit_num-1, cha_buf);
-}
-
-void shiftRegister4094SevenSeg::print(uint8_t _from, uint8_t _to, int _val){
-	String _str = (String) _val;
-	print(_from, _to, _str);
-}
-
-void shiftRegister4094SevenSeg::print(uint8_t _from, uint8_t _to, long _val){
-	String _str = (String) _val;
-	print(_from, _to, _str);
-}
-
-void shiftRegister4094SevenSeg::print(uint8_t _from, uint8_t _to, float _val){
-	String _str = (String) _val;
-	print(_from, _to, _str);
-}
-
-void shiftRegister4094SevenSeg::print(uint8_t _digit, char _ch){
-	setBufferArray(_digit, _ch);
-}
-
-void shiftRegister4094SevenSeg::print(uint8_t _from, uint8_t _to, String _str){
-	char cha_buf[_str.length()];
-	_str.toCharArray(cha_buf, _str.length());
-	setBufferArray(_from, _to, cha_buf);
-}
-
-void shiftRegister4094SevenSeg::clear(){
-	clearBuffer();
+	_str.toCharArray(cha_buf, _str.length()+1);
+	setDigitMapping(cha_buf, &_digit_buffer[0]);
 	shiftOutData(_digit_buffer);
 }
 
-String shiftRegister4094SevenSeg::getArrayString(){
-	String _str;
-	for(size_t i=0; i<_digit_num; i++){
-		_str+=_array_buffer[i];
+void shiftRegister4094SevenSeg::clear(){
+	char _zero_digit[_digit_num];
+	for (int i=0; i<_digit_num; i++) {
+		_zero_digit[i]=0;			// Off all segment
 	}
-	return _str;
+	shiftOutData(_zero_digit);
+}
+
+void shiftRegister4094SevenSeg::allOn(){
+	char _on_digit[_digit_num];
+	for (int i=0; i<_digit_num; i++) {
+		_on_digit[i]=0b11111111;	// On all segment and dot
+	}
+	shiftOutData(_on_digit);
 }
 
 /*-------------------------------------------- Private Function --------------------------------------------*/
 
-void shiftRegister4094SevenSeg::shiftOutData(byte segment_buffer[]){
-	/* - shift data to shift register.
-	 * - digit count from left to right.
-	 * - write last digit first.
-	 */
+void shiftRegister4094SevenSeg::shiftOutData(char segment_buffer[]){
 	digitalWrite(_str_pin, 0);
-	for(size_t i=_digit_num-1; i>=0; i--){
+	for(int i=_digit_num-1; i>=0; i--){
+		// Debugging
+		if(_debug) {
+			Serial.print("shiftout: "); Serial.println(segment_buffer[i], BIN);
+		} 
+
 		if(_seg_type) shiftOut(_data_pin, _clk_pin, MSBFIRST, segment_buffer[i]);	//Common cathode
 		else shiftOut(_data_pin, _clk_pin, MSBFIRST, ~segment_buffer[i]);			//Common anode
 	}
 	digitalWrite(_str_pin, 1);
 }
 
-void shiftRegister4094SevenSeg::clearBuffer(){
-	for(size_t i=0; i<_digit_num; i++){
-		_array_buffer[i] = SEG_BLANK_ARRAY; // set each element to 0.
-		_digit_buffer[i] = digitCodeMap[SEG_BLANK_INDEX];
+void shiftRegister4094SevenSeg::setDigitMapping(char ch[], char *digit){
+	for (size_t k=0; k<_digit_num; k++) {
+		digit[k] = 0;	// Reset buffer.
+	} 
+	int _start_idx = _digit_num-strlen(ch);
+	for (int i = 0; i < strlen(ch); ++i) {
+		if(ch[i]=='.') _start_idx++;	// '.' is not a digit count.
 	}
-}
 
-void shiftRegister4094SevenSeg::setBufferArray(uint8_t _from, uint8_t _to, char ch[]){
-	int _ch_last_index = sizeof(ch) - 2;
-	for(size_t i=_to; i<=_from; i--){
-		if (_ch_last_index >= 0) {
-			_array_buffer[i] = ch[_ch_last_index];
-			_ch_last_index--;
-		}else{
-			_array_buffer[i] = SEG_BLANK_ARRAY;
-		}
+	// Debugging
+	if(_debug){
+		Serial.print("array length: "); Serial.println(strlen(ch));
 	}
-	shiftOutData(_digit_buffer);
-}
-
-void shiftRegister4094SevenSeg::setBufferArray(uint8_t _digit, char _ch){
-	_array_buffer[_digit] = _ch;
-	shiftOutData(_digit_buffer);
-}
-
-void shiftRegister4094SevenSeg::setDigitMapping(char ch[]){
-	uint8_t _cha_index = 0;
-	byte _seg = digitCodeMap[SEG_BLANK_INDEX];
-	for(size_t _digit_index=0; _digit_index<_digit_num; _digit_index++){
-		const char _char = ch[_cha_index];
+	
+	int _cha_idx = 0;
+	for(int _digit=_start_idx; _digit<_digit_num; _digit++){
+		char _char = ch[_cha_idx];
 		if(_char>='0' && _char<='9'){
-			_seg = digitCodeMap[_char-'A'];
+			digit[_digit] = digitCodeMap[_char- '0'];
 		}else if(_char>='A' && _char<='Z'){
-			_seg = digitCodeMap[_char - 'A' + SEG_LETTER_INDEX];
+			digit[_digit] = digitCodeMap[_char - 'A' + SEG_LETTER_INDEX];
 		}else if(_char>='a' && _char<='z'){
-			_seg = digitCodeMap[_char - 'a' + SEG_LETTER_INDEX];
+			digit[_digit] = digitCodeMap[_char - 'a' + SEG_LETTER_INDEX];
 		}else if(_char == '-'){
-			_seg = digitCodeMap[SEG_DASH_INDEX];
+			digit[_digit] = digitCodeMap[SEG_DASH_INDEX];
 		}else if(_char == '*'){
-			_seg = digitCodeMap[SEG_DEGREE_INDEX];
+			digit[_digit] = digitCodeMap[SEG_DEGREE_INDEX];
 		}else if(_char == '.'){
-			_seg = digitCodeMap[SEG_PERIOD_INDEX];
+			digit[_digit] = digitCodeMap[SEG_PERIOD_INDEX];
 		}else{
-			_seg = digitCodeMap[SEG_BLANK_INDEX];
+			digit[_digit] = digitCodeMap[SEG_BLANK_INDEX];
 		}
-		_digit_buffer[_digit_index] = _seg; //
-		_cha_index++;
+		_cha_idx++;
 
 		//Add '.' to current digit and move to next.
-		if (_char == '.'){
-			_digit_buffer[_digit_index] |= digitCodeMap[SEG_PERIOD_INDEX];
-			_cha_index++;
+		if (ch[_cha_idx] == '.'){
+			digit[_digit] |= digitCodeMap[SEG_PERIOD_INDEX];
+			_cha_idx++;
 		}
 	}
+
 }
--- shiftRegister4094SevenSeg.h
+++ shiftRegister4094SevenSeg.h
@@ -3,44 +3,37 @@
 
 #include "Arduino.h"
 
-#define CATHODE 1
-#define ANODE 	0
+#define CATHODE 	1
+#define ANODE 		0
+
 
 class shiftRegister4094SevenSeg{
 public:
 	shiftRegister4094SevenSeg(bool type, uint8_t digit_num);
 	void begin(uint8_t data_pin, uint8_t clock_pin, uint8_t strobe_pin);
 	
+	void clear();
 	void print(char _ch);
 	void print(int _val);
 	void print(long _val);
 	void print(float _val);
 	void print(String _str);
 
-	void print(uint8_t _digit, char _ch);
-	void print(uint8_t _from, uint8_t _to, int _val);
-	void print(uint8_t _from, uint8_t _to, long _val);
-	void print(uint8_t _from, uint8_t _to, float _val);
-	void print(uint8_t _from, uint8_t _to, String _str);
-
-	void clear();
-	String getArrayString();
+	void allOn();
 
 private:
+	bool _debug = false;
+
 	bool 	_seg_type;
 	uint8_t _digit_num;
 	uint8_t _data_pin;
 	uint8_t _clk_pin;
 	uint8_t _str_pin;
 
-	char _array_buffer[];
-	byte _digit_buffer[];
+	char _digit_buffer[];	// Store binary mapping code
 
-	void shiftOutData(byte segment_buffer[]);
-	void clearBuffer();
-	void setBufferArray(uint8_t _from, uint8_t _to, char ch[]);
-	void setBufferArray(uint8_t _digit, char _ch);
-	void setDigitMapping(char ch[]);
+	void setDigitMapping(char ch[], char *digit);
+	void shiftOutData(char segment_buffer[]);
 };
 
 #endif
